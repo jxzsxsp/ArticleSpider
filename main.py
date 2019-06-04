560af47d065303d2d6b00a3cda12c06be405cdd0
Sean.Xiao <jxzsxsp@qq.com>
2019-06-04 09:41:41 +0800

    init project

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Sean'
+
+from scrapy.cmdline import execute
+
+import sys
+import os
+
+if __name__ == '__main__':
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    execute(["scrapy", "crawl", "jobbole"])
